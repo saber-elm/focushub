b4c4f002ad8e03a1d654c486383cf5f3232672fe
saber-elm <saber.elmbaz@gmail.com>
2025-04-26 11:28:28 +0200

    Edit Homepage Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link'
+
 // app/page.tsx
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center text-2xl">
-      Welcome to FocusHub 🚀
+      Welcome to
+      <p>&nbsp;</p>
+      <Link href={'/tasks'}>FocusHub</Link>
+      <p>&nbsp;</p>
+      🚀
     </main>
   )
 }
